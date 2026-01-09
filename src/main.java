b215c37bfd90a42550d0db712e0bdb5bd44cd974
lakshmisaranyakommuru-max <lakshmisaranyakommuru@gmail.com>
2026-01-09 17:13:37 +0530

    main.java

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+
+public class Main {
+  public static void main(string[] args) {
+    Scanner scanner = new Scanner(system.in);
+    int num1 = scanner.nextInt();
+    int num2 = scanner.nextInt();
+    int sum = num1 + num2;
+    System.out.println(sum);
+    scanner.close();
+  }
+}
