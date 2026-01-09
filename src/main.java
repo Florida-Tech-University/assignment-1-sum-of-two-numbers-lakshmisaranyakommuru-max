49c4e498d9a805be0b1559e7e985639e755c2d99
lakshmisaranyakommuru-max <lakshmisaranyakommuru@gmail.com>
2026-01-09 17:35:25 +0530

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,8 +1,8 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(string[] args) {
-    Scanner scanner = new Scanner(system.in);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     int num1 = scanner.nextInt();
     int num2 = scanner.nextInt();
     int sum = num1 + num2;
